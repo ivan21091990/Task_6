fab0b7f4645d01f8424887d7bf0d6f582aba8283
IPAndreev <aip1990andreev@yandex.ru>
2019-09-21 00:01:11 +0300

    finished task

--- src/com/itProger/Main.java
+++ src/com/itProger/Main.java
@@ -1,37 +1,39 @@
 package com.itProger;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Set;
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         String textOutput;
-        ArrayList<String> arrayList= new ArrayList<>();
-            BufferedReader br = new BufferedReader(new FileReader("aplana.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("aplana.txt"));
 
-         while ((textOutput = br.readLine()) != null){ {
-                System.out.println(textOutput);
-                System.out.println();}
-                for ( String str: textOutput.split(" ")){
-                    arrayList.add(str);
+        while ((textOutput = br.readLine()) != null) {
+            {
+                System.out.println("1. " + textOutput);
+                List<String> list = Arrays.asList(textOutput.split(" "));
+                Collections.sort(list);
+                System.out.println("2. " + list);
+
+                Map<String, Integer> map = new HashMap<String, Integer>(list.size());
+
+                for (String str : list) {
+                    Integer count = map.get(str);
+                    map.put(str, count == null ? 1 : count + 1);
                 }
-             Collections.sort(arrayList);
-                for (String a: arrayList){
-                    int i =0;
-                    for (String b: arrayList){
-                        if (b.equals(a)) i++;
+                System.out.println("3. " + map);
+
+                String s = null;
+                int p = 0;
+                for (String w : map.keySet()) {
+                    if (p < map.get(w)) {
+                        p = map.get(w);
+                        s = w;}
                     }
-                    System.out.println(a + "   " + i);
+                System.out.println("4. Самое повторяющееся слово: " + s + " (встречается " + map.get(s) + " раз(а))");
                 }
-
-
             }
-
-
-
-
+        }
     }
-}
